592328ab57a8a4b9827cbf13ff3ec31b1f21a63b
Christina Nkya <christina.nkya@sdgku.edu>
2024-08-01 23:01:20 -0400

    Class 03

---
+++ app/database/__init__.py
@@ -0,0 +1,13 @@
+#use the g function which stands for global
+#Initalize connection to database
+
+from flask import g
+import sqlite3
+
+DATABASE_URL = "main.db"
+
+def get_db():
+db = getattr(g, "_database", None)
+if not db:
+    db = g._database = sqlite3.connect(DATABASE_URL)
+    return db
---
+++ setup.sql
@@ -0,0 +1,31 @@
+CREATE TABLE task(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128),
+    summary VARCHAR(256),
+    description TEXT,
+    is_done BOOLEAN DEFAULT 0
+);
+
+--Insert some dummy data to test the tabel
+
+INSERT INTO task(
+    name,
+    summary,
+    description
+    
+)VALUES(
+    "Wash car",
+    "The car needs to be washed",
+    "Make sure the car gets waxed after it is washed"
+    
+),(
+    "Walk the dog",
+    "Rocky needs their exercise",
+    "Make sre to do at least 2 laps around the park"
+    
+),(
+    "Buy Groceries",
+    "Go to the supermarket and buy groceries",
+    "We need: Eggs, Milk, Cereal, and Coffee"
+    
+);
